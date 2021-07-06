ed46215637dd408b840ad0da87de9d217a77596f
hanneloreelsden <85871967+hanneloreelsden@users.noreply.github.com>
2021-07-06 15:44:51 +0100

    Add files via upload

---
+++ identifyHash.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace identifyHash
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter hash to identify: ");
+            string hash = Console.ReadLine(); 
+            if(hash.Length == 32)
+            {
+                Console.WriteLine(hash + " is an MD5 hash");
+                Console.ReadLine();
+            }
+            if(hash.Length == 64)
+            {
+                Console.WriteLine(hash + " is a SHA-256 hash");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
