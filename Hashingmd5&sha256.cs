9019d70c64434038044593fdf1083a5355b7e7b8
hanneloreelsden <85871967+hanneloreelsden@users.noreply.github.com>
2021-07-06 11:05:12 +0100

    Add files via upload

---
+++ Hashingmd5&sha256.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashing2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter text: ");
+            string str = Console.ReadLine();
+            var text = (str);
+            
+            using (var md5 = MD5.Create())
+            {
+                var sourceBytes = Encoding.UTF8.GetBytes(text);
+                var hashBytes = md5.ComputeHash(sourceBytes);
+                var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+                Console.WriteLine("The MD5 hash of " + text + " is: " + hash);
+
+            }
+            using (var sha256 = SHA256.Create())
+            {
+                var sourceBytes = Encoding.UTF8.GetBytes(text);
+                var hashBytes2 = sha256.ComputeHash(sourceBytes);
+                var hash2 = BitConverter.ToString(hashBytes2).Replace("-", string.Empty);
+                Console.WriteLine("The SHA-256 hash of " + text + " is: " + hash2);  
+            }
+
+        }   
+    }
+}
