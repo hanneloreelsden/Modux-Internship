8715f38fabc2ce3fa0ac8d4cf94c09fbefc5846b
hanneloreelsden <85871967+hanneloreelsden@users.noreply.github.com>
2021-07-06 15:44:28 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,27 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Text;
+using System.Net;
+using System.IO;
 
-namespace StrToByte
+namespace identifyHash
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Enter text: ");
-            string text = Console.ReadLine(); 
-            byte[] barr= Encoding.UTF8.GetBytes(text);
-            Console.WriteLine(barr);
+            Console.WriteLine("Enter hash to identify: ");
+            string hash = Console.ReadLine(); 
+            if(hash.Length == 32)
+            {
+                Console.WriteLine(hash + " is an MD5 hash");
+                Console.ReadLine();
+            }
+            if(hash.Length == 64)
+            {
+                Console.WriteLine(hash + " is a SHA-256 hash");
+                Console.ReadLine();
+            }
             Console.ReadLine();
         }
     }
