f923c4153268e6d239655a4c7e48974df87811e5
hanneloreelsden <85871967+hanneloreelsden@users.noreply.github.com>
2021-07-07 12:30:55 +0100

    Add files via upload

--- identifyHash.cs
+++ identifyHash.cs
@@ -6,9 +6,9 @@ using System.IO;
 
 namespace identifyHash
 {
-    class Program
+    class idHash
     {
-        static void Main()
+        private void identifyHash()
         {
             Console.WriteLine("Enter hash to identify: ");
             string inputhash = Console.ReadLine(); 
@@ -22,37 +22,15 @@ namespace identifyHash
                 Console.WriteLine(inputhash + " is a SHA-256 hash");
                 Console.ReadLine();
             }
-            Console.ReadLine();
-            // Start of new section
-            string[] lines = File.ReadAllLines(@"/Users/hanneloreelsden/Desktop/PwnedPasswordsTop100k.txt");  
-            foreach (string line in lines)
-            {
-                var password = (line);
-
-                using (var md5 = MD5.Create())
-                {
-                    var sourceBytes = Encoding.UTF8.GetBytes(password);
-                    var hashBytes = md5.ComputeHash(sourceBytes);
-                    var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
-                    if(hash.Equals(inputhash))
-                    {
-                        Console.WriteLine("The password of " + hash + " is: " + password);
-                        Console.ReadLine();
-                    }
-                }
-                
-                using (var sha256 = SHA256.Create())
-                {
-                    var sourceBytes = Encoding.UTF8.GetBytes(password);
-                    var hashBytes2 = sha256.ComputeHash(sourceBytes);
-                    var hash2 = BitConverter.ToString(hashBytes2).Replace("-", string.Empty);
-                    if(hash2.Equals(inputhash))
-                    {
-                        Console.WriteLine("The password of " + hash2 + " is :" + password);
-                        Console.ReadLine();
-                    }
-                }
-            }
+        }
+       
+        static void Main()
+        {
+            //creating object of class idHash
+            idHash p = new idHash();
+            p.identifyHash(); // Calling method
+            Console.ReadLine();        
+        
         }
     }
 }
