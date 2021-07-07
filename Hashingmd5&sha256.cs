7c81a2ed72b2548bff81a4d64382c7123546175b
hanneloreelsden <85871967+hanneloreelsden@users.noreply.github.com>
2021-07-07 14:21:17 +0100

    Add files via upload

--- Hashingmd5&sha256.cs
+++ Hashingmd5&sha256.cs
@@ -6,26 +6,26 @@ using System.Net;
 namespace Hashing2
 {
     class calcHash
-    {
-            
-        private void calculateMD5()
+    {   
+        static void calculateMD5()
         {
+            Console.WriteLine("Enter text to convert to MD5: ");
+            string str = Console.ReadLine();
+            var text = (str);
             using (var md5 = MD5.Create())
             {
-                Console.WriteLine("Enter text: ");
-                string str = Console.ReadLine();
-                var text = (str);
                 var sourceBytes = Encoding.UTF8.GetBytes(text);
                 var hashBytes = md5.ComputeHash(sourceBytes);
                 var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
                 Console.WriteLine("The MD5 hash of " + text + " is: " + hash);
-
+                Console.ReadLine();
             }
         }
         
-        private void calculateSHA256()
+        static void calculateSHA256()
         {
-            Console.WriteLine("Enter text: ");
+            // Here, need to solve error which comes as not displaying text "SHA256"
+            Console.WriteLine("Enter text to convert to SHA-256: ");
             string str1 = Console.ReadLine();
             var text1 = (str1);
             using (var sha256 = SHA256.Create())
@@ -33,16 +33,15 @@ namespace Hashing2
                 var sourceBytes2 = Encoding.UTF8.GetBytes(text1);
                 var hashBytes2 = sha256.ComputeHash(sourceBytes2);
                 var hash2 = BitConverter.ToString(hashBytes2).Replace("-", string.Empty);
-                Console.WriteLine("The SHA256 hash of " + text1 + " is: " + hash2);  
-            }
+                Console.WriteLine("The SHA256 hash of " + text1 + " is: " + hash2);
+                Console.ReadLine(); 
+            } 
         }
 
         static void Main()
-        {
-            //creating object of calcHash
-            calcHash p = new calcHash();
-            p.calculateMD5(); // Calling method
-            p.calculateSHA256(); // Calling method
+        { 
+            calculateMD5(); // Calling method
+            calculateSHA256(); // Calling method
             Console.ReadLine(); 
         }   
     }
