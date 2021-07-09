ce201cb885602e2ea555f106446c54b7b3d15407
hanneloreelsden <85871967+hanneloreelsden@users.noreply.github.com>
2021-07-09 17:44:47 +0100

    Add files via upload

--- Cisco_Python_work.cs
+++ Cisco_Python_work.cs
@@ -15,22 +15,20 @@ namespace Cisco_Python_work
             int index = Int32.Parse(pw.Substring(0,2));
             string enc_pw = pw.Substring(2).Trim();
             Console.WriteLine(enc_pw);
-            string hex_pw = string.Empty;
-            //Here is where code goes wrong from Python script
-            for (int i = 0; i < enc_pw.Length ; i += 2)
+            var hex_pw = string.Empty;
+            int i = 0;
+            while ((i+=2)< enc_pw.Length)
             {
-                hex_pw+=(enc_pw.Substring(i, 2));
-                hex_pw.Split(',');
+                hex_pw+=(enc_pw.Substring(i,2));
             }
             Console.WriteLine(hex_pw);
-            //need to fix above
             // Create the cleartext list
             string cleartext = string.Empty;
             for(int j=0; j<hex_pw.Length; j+=1)
             {
                 int cur_index = (j+index) % 53;
                 var cur_salt = Char.ConvertToUtf32(salt,cur_index);
-                int cur_hex_int = Convert.ToInt32(hex_pw.Substring(j,0));
+                int cur_hex_int = Int32.Parse(hex_pw.Substring(j, j));
                 var cleartext_char = cur_salt ^ cur_hex_int;  
                 cleartext+=(cleartext_char);
             }
